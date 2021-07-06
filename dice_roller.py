24dad1acc859f9f2ff2b31f10b5919da907add0d
NandithaNagulapally <nanditha.nagulapally9@gmail.com>
2021-07-06 18:59:19 +0530

    second one

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,13 @@
-def main():
-  print('You rolled a die')
-
-if __name__== "__main__":
-  main()
+import random
+dice_rolls = 2
+dice_sum = 0
+for i in range(0,dice_rolls):
+#def main():
+  roll = random.randint(1,6)
+  dice_sum += roll
+  print(f'You rolled a {roll}')
+print(f'YOu have rolled a total {dice_sum}')
+  #variable_sum = variable_1 + variable_2
+  
+#if __name__== "__main__":
+ # main()
